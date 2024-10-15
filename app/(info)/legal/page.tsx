57258cee198eaa2df03b39e593c1b1fa8e4781f6
SuyashC1000 <107310104+SuyashC1000@users.noreply.github.com>
2024-10-15 07:19:37 +0530

    Updated Tos and Privacy Policy pages

--- app/(info)/legal/page.tsx
+++ app/(info)/legal/page.tsx
@@ -1,10 +1,31 @@
+import { Heading, Link, ListItem, Text, UnorderedList } from "@chakra-ui/react";
 import React from "react";
 
 const LegalPage = () => {
   return (
     <div>
       <title>Legal - OpenMock</title>
-      LegalPage
+
+      <Heading size={"md"}>Legal</Heading>
+      <Text>
+        This is the legal section of the site. Choose to visit the following
+        pages from here:
+      </Text>
+      <br />
+      <UnorderedList>
+        <ListItem>
+          <Link href="/legal/terms-of-service">
+            <Text>Terms of Service</Text>
+          </Link>
+        </ListItem>
+        <ListItem>
+          <Link href="/legal/privacy">
+            <Text>Privacy Policy</Text>
+          </Link>
+        </ListItem>
+      </UnorderedList>
+      <br />
+      <Text>This page is currently under development</Text>
     </div>
   );
 };
